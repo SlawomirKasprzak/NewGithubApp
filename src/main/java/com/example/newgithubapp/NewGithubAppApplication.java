caecb79e36955210d934d9807df28163cdb1e3f2
Slawomir Kasprzak <slawek.kasprzak@icloud.com>
2024-07-25 22:43:33 +0100

    created GithubProxy

--- src/main/java/com/example/newgithubapp/NewGithubAppApplication.java
+++ src/main/java/com/example/newgithubapp/NewGithubAppApplication.java
@@ -1,13 +1,26 @@
 package com.example.newgithubapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NewGithubAppApplication {
+
+    @Autowired
+    GithubProxy githubProxy;
 
     public static void main(String[] args) {
         SpringApplication.run(NewGithubAppApplication.class, args);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void makeRequestToGithubEndPoint() {
+
     }
 
 }
