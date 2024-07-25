ff2499fbfed4a18083cc94f644ea0e5b9125ff51
Slawomir Kasprzak <slawek.kasprzak@icloud.com>
2024-07-25 21:52:54 +0100

    first commit

---
+++ src/main/java/com/example/newgithubapp/NewGithubAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.newgithubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGithubAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewGithubAppApplication.class, args);
+    }
+
+}
